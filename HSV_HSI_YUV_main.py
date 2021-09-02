b36f39f443a241b23e4c5c5f76653f3dd6dab0d1
leventkalkavan <87976726+leventkalkavan@users.noreply.github.com>
2021-09-02 09:29:16 +0300

    Update HSV_HSI_YUV_main.py

--- HSV_HSI_YUV_main.py
+++ HSV_HSI_YUV_main.py
@@ -20,6 +20,16 @@ def HSI():
 def HSI_yeni():
    return render_template("HSI_yeni.htm") 
 
+@app.route("/YUV")
+def YUV():
+    return render_template ("YUV.htm")
+
+@app.route('/YUV_yeni')
+def YUV_yeni():
+   return render_template ("YUV_yeni.htm")
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)   
 
@@ -126,6 +136,38 @@ for i in range(w):
       resim_pix[i,j] = (h, s, v)
 #Fonksiyondan elde edilen  h,s,i değerleri görüntünün uygun piksellerine yeni değer olarak atılır.
 resim.save("static\img\HSI_yeni.jpg")
+#Oluşan görüntü yeni adıyla belirtilen klasöre kaydedilir.
+resim.show()
+#Görüntü ekranda gösterilir.
+def  rgbtoyuv (R, G, B):
+  r=R/255
+  g=G/255 #Değerler normalize hale getirilir.
+  b=B/255
+
+  Y=(0.299*r)+(0.587*g)+(0.114*b)
+  U=0.492*(b-Y)
+  V=0.877*(r-Y)
+
+  return int(Y*255),int(U*255),int(V*255) # y,u,v değerleri 255 ile çarpılarak resim üzerinde gösterilir.
+  #Tür dönüşümü yapılan değerler geri gönderilir.
+resim=Image.open("static\img\YUV.jpg").convert("RGB")
+#resim değişkeninin içerisine belirtilen klasörde bulunan 'HSV.jpg' adlı resim dosyası atılır.
+resim_pix = resim.load()
+#resim_pix adlı yeni bir matris oluşturup bunun içerisine resim dosyası yüklenir.
+w=resim.size[0]
+#w değişkenine resim dosyasının genişlik boyutunu atılır. Dosyanın genişliği 640 piksel.
+hg=resim.size[1]
+#hg değişkenine resim dosyasının yükseklik boyutunu atılır. Dosyanın yüksekliği 360 piksel.
+for i in range(w):
+#Görüntünün tüm piksellerinde bulunan renk kodlarını almak için iç içe döngü oluşturulur.
+  for j in range(hg):
+      r, g, b = resim.getpixel((i, j))
+#Her pikselde birer r(kırmızı),g(yeşil),b(mavi) değeri bulunmaktadır. Bunlar o piksele renk veren kodlardır. Öncelikle 'getpixel' fonksiyonu ile bu değerleri r,g,b değişkenlerine atılır.
+      h, s, v = rgbtoyuv(r, g, b)
+#Alınan r,g,b değişkenler rgbtoyuv fonksiyonuna gönderilir. Bu fonksiyonun geri dönüşü üç parametre olacaktır. Bunlar da y,u,v değişkenlerinde saklanır.
+      resim_pix[i,j] = (h, s, v)
+#Fonksiyondan elde edilen  y,u,v değerleri görüntünün uygun piksellerine yeni değer olarak atılır.
+resim.save("static\img\YUV_yeni.jpg")
 #Oluşan görüntü yeni adıyla belirtilen klasöre kaydedilir.
 resim.show()
 #Görüntü ekranda gösterilir.
